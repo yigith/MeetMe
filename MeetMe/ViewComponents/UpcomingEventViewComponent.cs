577912eea39d052b22189579dcc1eff689b602f4
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2021-01-28 13:16:45 +0300

    upcoming viewcomponent

---
+++ MeetMe/ViewComponents/UpcomingEventViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using MeetMe.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeetMe.ViewComponents
+{
+    // https://docs.microsoft.com/en-us/aspnet/core/mvc/views/view-components?view=aspnetcore-5.0
+    public class UpcomingEventViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UpcomingEventViewComponent(ApplicationDbContext dbContext)
+        {
+            _db = dbContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var meeting = await _db.Meetings
+                .Where(x => x.MeetingTime > DateTime.Now)
+                .OrderBy(x => x.MeetingTime)
+                .FirstOrDefaultAsync();
+
+            return View(meeting);
+        }
+    }
+}
